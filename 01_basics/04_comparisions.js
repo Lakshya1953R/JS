a5e8c25c314a4125342c8b50ac49c10d4645bdfe
Lakshya1953R <lakshyagarg224@gmail.com>
2025-06-01 16:39:48 +0000

    comparisions and datatypes summary

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2>=1); // >= means greater than or  equalto
+// console.log(2==1); // == means equal to
+// console.log(2!=1); // != means not equal to  
+
+
+
+console.log("2">1)
+console.log("02">1);
+
+console.log(null>0) // comparisions like <,>,<= 
+// convert null into a number treating it as 0
+console.log(null==0) // equality check (==) does not converts null into a number 
+console.log(null>=0);
+// same does not happens for "undefined" it is not 
+// converted into number by any comparsion
+
+
+// === means strict check that is it also check the value`s datatype
+
+console.log("2" === 2);
+// it gives false because as it checks the datatypes are different 
+// which means they are not compareable hence the output is false
---
+++ datatypes_summary.js
@@ -0,0 +1,34 @@
+//  Primitive 
+
+// 7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedin = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId);
+
+const bigNumber = 3456789123456789n
+
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heroes = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "lakshya",
+    age : 18,
+}
+
+const myFunction = function(){
+    console.log("hello world"); 
+}
+console.log(typeof myFunction );
