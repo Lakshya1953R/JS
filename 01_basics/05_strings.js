9c03ba906d1aad8d7f9eb67c257929166c59e945
Lakshya1953R <lakshyagarg224@gmail.com>
2025-06-06 18:03:43 +0000

    Strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,10 +1,33 @@
 const name = "lakshya"
 const repoCount =  50
 
-// console.log(name + repoCount + "value");
+// console.log(name +  repoCount + " value");
 
 console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('lakshya')
+const gameName = new String('laks-hya')
 
-console.log(gameName[0]);
+console.log(gameName [2]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase('l'))
+console.log(gameName.charAt(2)); // used to know at what place is which word 
+console.log(gameName.indexOf('l')); // to know the place number of the word 
+
+const newString = gameName.substring(0, 3); // the word at 3rd place is not inlcuded
+console.log(newString);
+
+
+const anotherString = gameName.slice(-5, 3)
+console.log(anotherString)
+const newStringOne = "   lakshya "
+console.log(newStringOne.trim()); // removes the spaces in the string at start and end
+
+const url = "https://lakshya.com/lakshya%20garg"
+
+console.log(url.replace('%20', '-'));
+console.log(url.includes('lak'))
+
+console.log(gameName.split('a'))
